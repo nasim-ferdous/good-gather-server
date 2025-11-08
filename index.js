bfa5af81599d72ba39a6d1c4ee99db6b1a393f79
nasim-ferdous <nasimferdouspust@gmail.com>
2025-11-08 23:58:24 +0600

    created server and connected to mongodb

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+// password= AdZXaZsB6XGSfnxR
+// user = good-gatherdb
+
+const uri =
+  "mongodb+srv://good-gatherdb:AdZXaZsB6XGSfnxR@cluster0.picyulc.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Good Gather server is running");
+});
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Good Gather app is listening on ${port}`);
+});
